ab1e34583bb3c42e67024689c55a18281b52be14
jaisingh-25 <113584679+jaisingh-25@users.noreply.github.com>
2023-08-01 08:58:50 +0530

    Update Alphabet Patterns.py

--- Alphabet Patterns.py
+++ Alphabet Patterns.py
@@ -1,72 +1,72 @@
-# l=[]
-# for i in range(1,51):
-#     l.append(i**2)
-# t=tuple(l)
-# print(t)
+l=[]
+for i in range(1,51):
+    l.append(i**2)
+t=tuple(l)
+print(t)
 
-# print()
-# print()
+print()
+print()
 
-# l=[]
-# for i in range(97,123):
-#     l.append(chr(i)*(i-96))
-# t=tuple(l)
-# print(t)
+l=[]
+for i in range(97,123):
+    l.append(chr(i)*(i-96))
+t=tuple(l)
+print(t)
 
-# print()
-# print()
+print()
+print()
 
-# a=[]
-# for i in range(0,50):
-#     l=[i]
-#     a=a+l
-# print(a)
+a=[]
+for i in range(0,50):
+    l=[i]
+    a=a+l
+print(a)
 
-# print()
+print()
 
-# a=[]
-# for i in range(1,50):
-#     b=i**2
-#     l=[b]
-#     a=a+l
-# print(a)
+a=[]
+for i in range(1,50):
+    b=i**2
+    l=[b]
+    a=a+l
+print(a)
 
-# print()
+print()
 
-# a=[]
-# for i in range(65,91):
-#     b=chr(i)
-#     c=i-64
-#     l=[b*c]
-#     a=a+l
-# print(a)
+a=[]
+for i in range(65,91):
+    b=chr(i)
+    c=i-64
+    l=[b*c]
+    a=a+l
+print(a)
 
-# print()
-# print()
+print()
+print()
 
-# n=int(input("Enter no. of rows - "))
-# for i in range(65,65+n):
-#     for j in range(65,i+1):
-#         print(chr(j), end=" ")
-#     print()
+n=int(input("Enter no. of rows - "))
+for i in range(65,65+n):
+    for j in range(65,i+1):
+        print(chr(j), end=" ")
+    print()
 
-# print()
-# print()
+print()
+print()
 
-# n=int(input("Enter no. of rows - "))
-# for i in range(65,65+n):
-#     print("  "*(n+64-i),end="")
-#     for j in range(65,i+1):
-#         print(chr(j), end=" ")
-#     for j in range(i,65,-1):
-#         print(chr(j-1), end=" ")
-#     print()
+n=int(input("Enter no. of rows - "))
+for i in range(65,65+n):
+    print("  "*(n+64-i),end="")
+    for j in range(65,i+1):
+        print(chr(j), end=" ")
+    for j in range(i,65,-1):
+        print(chr(j-1), end=" ")
+    print()
 
-# print()
-# print()
+print()
+print()
 
-# n=int(input("Enter no. of rows - "))
-# for i in range(n,0,-1):
-#     for j in range(i+64,65+n):
-#         print(chr(j), end="")
-#     print()
+n=int(input("Enter no. of rows - "))
+for i in range(n,0,-1):
+    for j in range(i+64,65+n):
+        print(chr(j), end="")
+    print()
